0dbc28a8306eac249663df4bb65b3291a134847c
Leo Jiang <leo.jiang.dev@gmail.com>
2012-04-01 23:22:49 +0800

    add license info and parameter checking in the head.

--- src/tools/create-corpus-dir.sh
+++ src/tools/create-corpus-dir.sh
@@ -1,4 +1,16 @@
-#! /bin/bash
+#!/bin/bash
+
+# This file is part of Moses Suite.
+# Author: Leo Jiang <leo.jiang.dev@gmail.com>
+# Copyright 2012 Leo Jiang
+# under GPL License.
+
+E_BARARGS=65
+
+if [ $# -ne 2 ]; then
+    echo "Usage: `basename $0` src-lang target-lang"
+    exit $E_BARARGS
+fi
 
 src=$( echo "$1" | tr '[:lower:]' '[:upper:]' )
 target=$( echo "$2" | tr '[:lower:]' '[:upper:]' )
