253da137f56d9010074979ef15981ac1a32f0734
Leo Jiang <leo.jiang.dev@gmail.com>
2012-04-01 22:49:24 +0800

    fix a spelling-wrong bug in create-corpus-dir.sh.

--- src/tools/create-corpus-dir.sh
+++ src/tools/create-corpus-dir.sh
@@ -3,6 +3,6 @@
 src=$( echo "$1" | tr '[:lower:]' '[:upper:]' )
 target=$( echo "$2" | tr '[:lower:]' '[:upper:]' )
 
-mkdir -p /data/corpus/${src}-${target}/{trainning,tuning,recaser,evaluation}
+mkdir -p /data/corpus/${src}-${target}/{training,tuning,recaser,evaluation}
 cd /data/corpus/
 ln -s ${src}-${target} ${target}-${src}
