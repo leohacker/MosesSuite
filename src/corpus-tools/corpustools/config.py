65e9b6ce0aaad98ac485dc903cf5068d5fc3b438
Leo Jiang <leo.jiang.dev@gmail.com>
2012-06-18 11:49:25 +0800

    init the clean and config.

---
+++ src/corpus-tools/corpustools/config.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""Config module for corpus tools.
+
+This module define the class CorpusToolsConfig to access the configuration of 
+corpus tools. The instance will read the system-wide and user configuration
+automatically, then you specify a configuration file to read.
+"""
+
+import json
+import os.path
+import ConfigParser
+
+class CorpusToolsConfig(object):
+    """Class for reading corpus tools config.
+
+    /etc/corpustools.conf   system wide configuration.
+    ~/.corpustools.conf      user configuration.
+    -c config               specify the config file in command line.
+    """
+    SYSTEM_CONFIG = "/etc/corpustools.conf"
+    USER_CONFIG = "~/.corpustools.conf"
+
+    def __init__(self):
+        """Read the system wide and default user configuration."""
+        self.config = ConfigParser.SafeConfigParser()
+        configfiles = [ CorpusToolsConfig.SYSTEM_CONFIG,
+                        os.path.expanduser(CorpusToolsConfig.USER_CONFIG) ]
+
+        for configfile in configfiles:
+            self.readfile(configfile)
+
+    def readfile(self, filename):
+        """Read the configuration from file."""
+        # Needn't to catch the IOError when read config because ConfigParser will ignore the file if failed to open/access it.
+        # BUT should catch the exception of parser to avoid unexpected termination.
+        # e.g. system-wide config is broken, but I can't correct it because of permission.
+        try:
+            self.config.read(filename)
+        except ConfigParser.Error as e:
+            print e
+
+    def sections(self):
+        return self.config.sections()
+
+    def options(self, section):
+        if self.config.has_section(section):
+            return self.config.options(section)
+        else:
+            return None
+
+    def __getitem__(self, inquire_str):
+        section, option = inquire_str.split('.')
+        return self.config.get(section, option) if self.config.has_option(section, option) else None
+
+    def dump(self):
+        for section in self.config.sections():
+            print "[" + section + "]"
+            for name, value in self.config.items(section):
+                print name + " = " + value
+            print ""
+
+class CleanConfig(object):
+    def __init__(self, filename):
+        self._steps = None
+        try:
+            fp = open(filename, 'r')
+            self._steps = json.load(fp)
+        except IOError as e:
+            print e
+        except ValueError as e:
+            print e
+
+    @property
+    def steps(self):
+        return self._steps
+
+    @steps.setter
+    def steps(self, step_list):
+        self._steps = step_list
+
+    def has_steps(self):
+        return False if self._steps is None else True
+
+    @property
+    def source_lang(self):
+        return self._source_lang
+
+    @source_lang.setter
+    def source_lang(self, value):
+        self._source_lang = value
+
+    @property
+    def target_lang(self):
+        return self._target_lang
+
+    @target_lang.setter
+    def target_lang(self, value):
+        self._target_lang = value
+
+    @property
+    def infile_dir(self):
+        """The directory to store the corpus files should be cleaned."""
+        return self._infile_dir
+
+    @infile_dir.setter
+    def infile_dir(self, value):
+        self._infile_dir = value
+
+    @property
+    def outfile_dir(self):
+        """The directory to store the cleaned corpus files."""
+        return self._outfile_dir
+
+    @outfile_dir.setter
+    def outfile_dir(self, value):
+        self._outfile_dir = value
+
+    @property
+    def corpus_name(self):
+        """The corpus filename without language extension."""
+        return self._corpus_name
+
+    @corpus_name.setter
+    def corpus_name(self, value):
+        self._corpus_name = value
+
+    @property
+    def working_dir(self):
+        """The working directory for corpus clean tool to store intermediate
+        files."""
+        return self._working_dir
+
+    @working_dir.setter
+    def working_dir(self, value):
+        self._working_dir = value
