b368c9d805580cdd2ea745869efed7e0f0b5f86b
Leo Jiang <leo.jiang.dev@gmail.com>
2012-06-27 23:22:49 +0800

    cut the tuning and evaluation part from training script. and not clean the corpus again.

--- src/moses-suite-tools/moses-suite-irstlm-train.sh
+++ src/moses-suite-tools/moses-suite-irstlm-train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Train a model with IRSTLM support.
+# Train an IRSTLM model.
 #
 # Project:      Moses Suite
 # URL:          http://github.com/leohacker/MosesSuite
@@ -10,8 +10,6 @@
 
 # Usage:
 #       moses-suite-irstlm-train src-lang tar-lang id
-
-# May 16 2012   A simple script to train model with settings described same as moses core website.
 
 export LC_ALL=C
 
@@ -24,24 +22,24 @@ fi
 
 # check parameters number.
 if [ $# != 3 ]; then
-    echo "Usage: `basename $0` source_language target_language id"
+    echo "Usage: `basename $0` source_lang target_lang engine_id"
     exit $E_INVAL
 fi
 
 src_lang=$1
-tar_lang=$2
+tgt_lang=$2
 id=$3
 
 # -- Normalize the src and target language ID. --
 SRC=$( echo "$src_lang" | tr '[:lower:]' '[:upper:]' )
-TARGET=$( echo "$tar_lang" | tr '[:lower:]' '[:upper:]' )
+TGT=$( echo "$tgt_lang" | tr '[:lower:]' '[:upper:]' )
 
 src=$( echo "$SRC" | tr '[:upper:]' '[:lower:]' )
-target=$( echo "$TARGET" | tr '[:upper:]' '[:lower:]' )
+tgt=$( echo "$TGT" | tr '[:upper:]' '[:lower:]' )
 
 # Copyleft declaration
-version="1.0"
-echo "Moses Suite Train Model -- Author: Leo Jiang <leo.jiang.dev@gmail.com>"
+version="0.2"
+echo "Moses Suite Training Script -- Author: Leo Jiang <leo.jiang.dev@gmail.com>"
 echo "Version: $version"
 echo
 
@@ -53,7 +51,10 @@ source_moses_conf
 SCRIPTS_ROOT=${MOSES_SUITE_ROOT}/moses/scripts
 check_dir "${SCRIPTS_ROOT}" "Moses script root directory."
 
-TM_ROOT=${MOSES_DATA_ROOT}/translation_models/${SRC}-${TARGET}/${id}
+TM_ROOT=${MOSES_DATA_ROOT}/translation_models/${SRC}-${TGT}/${id}
+if [ -d "${TM_ROOT}" ]; then
+    rm -rf ${TM_ROOT}
+fi
 mkdir -p ${TM_ROOT}
 
 check_dir "${TM_ROOT}"  "root directory of translation model"
@@ -61,10 +62,8 @@ if [ ! -w "${TM_ROOT}" ]; then
     echo "No permission for writing the directory ${TM_ROOT}"
     exit $E_ACCES
 fi  
-echo "Clean the directory: $TM_ROOT"
-rm -rf ${TM_ROOT}/{bin-model,model,lm,training,tuning,truecase-model,evaluation,corpus}
 
-CORPUS_ROOT=${MOSES_DATA_ROOT}/corpus/${SRC}-${TARGET}
+CORPUS_ROOT=${MOSES_DATA_ROOT}/corpus/${SRC}-${TGT}
 check_dir "${CORPUS_ROOT}" "root directory of corpus in corpus repository."
 
 IRSTLM=${MOSES_SUITE_ROOT}/irstlm
@@ -73,20 +72,6 @@ export IRSTLM
 
 # Check the location of scritps and executable program.
 # =====================================================
-tokenizer=${SCRIPTS_ROOT}/tokenizer/tokenizer.perl
-lowercaser=${SCRIPTS_ROOT}/tokenizer/lowercase.perl
-detokenizer=${SCRIPTS_ROOT}/tokenizer/detokenizer.perl
-clean_corpus_n=${SCRIPTS_ROOT}/training/clean-corpus-n.perl
-train_truecaser=${SCRIPTS_ROOT}/recaser/train-truecaser.perl
-truecaser=${SCRIPTS_ROOT}/recaser/truecase.perl
-
-check_file "$tokenizer"     "European language tokenizer"
-check_file "$lowercaser"    "lowercaser"
-check_file "$detokenizer"   "detokenizer"
-check_file "$clean_corpus_n"    "script clean long sentence"
-check_file "$train_truecaser"   "script train truecase"
-check_file "$truecaser"     "truecaser"
-
 train_model=${SCRIPTS_ROOT}/training/train-model.perl
 moses=${MOSES_SUITE_ROOT}/moses/bin/moses
 mertdir=${MOSES_SUITE_ROOT}/moses/bin
@@ -94,111 +79,71 @@ mert_moses=${SCRIPTS_ROOT}/training/mert-moses.pl
 processPhraseTable=${MOSES_SUITE_ROOT}/moses/bin/processPhraseTable
 processLexicalTable=${MOSES_SUITE_ROOT}/moses/bin/processLexicalTable
 
-check_file  "$train_model"      "script train-model "
-check_file  "$moses"            "moses core"
-check_dir   "$mertdir"          "mert directory"
-check_file  "$mert_moses"       "script mert-moses"
+check_file  "$train_model"          "script train-model "
+check_file  "$moses"                "moses core"
+check_dir   "$mertdir"              "mert directory"
+check_file  "$mert_moses"           "script mert-moses"
 check_file  "$processPhraseTable"   "processPhraseTable"
 check_file  "$processLexicalTable"  "processLexicalTable"
-
-filter_model_given_input=${SCRIPTS_ROOT}/training/filter-model-given-input.pl
-check_file  "$filter_model_given_input" "script of filter model with given input"
-
-multi_bleu=${SCRIPTS_ROOT}/generic/multi-bleu.perl
-check_file "$multi_bleu" "script of bleu"
 
 # Prepare IRSTLM Corpus
 # ============================================
 cd ${TM_ROOT}
-mkdir -p corpus/{lm,training,tuning,evaluation,truecase}
-mkdir {lm,training,tuning,evaluation,truecase-model}
+mkdir -p corpus/{lm,training,truecase}
+mkdir {lm,training,truecase-model}
 
 # prepare training corpus.
 # ------------------------
-corpus_truecase=corpus_case
+#corpus_truecase=corpus_case
 #corpus_lm=corpus_lm
 corpus_training=corpus_training
-corpus_tuning=corpus_tuning
-corpus_eval=corpus_eval
 
-cd ${TM_ROOT}/corpus/truecase/
-check_file ${CORPUS_ROOT}/truecase/${corpus_truecase}.${src} "truecase corpus ${src}"
-check_file ${CORPUS_ROOT}/truecase/${corpus_truecase}.${target} "truecase corpus ${target}"
-cp ${CORPUS_ROOT}/truecase/${corpus_truecase}.${src} .
-cp ${CORPUS_ROOT}/truecase/${corpus_truecase}.${target} .
-
-# train truecase model.
-cd ${TM_ROOT}/truecase-model/
-$train_truecaser --model truecase-model.${src} --corpus ${TM_ROOT}/corpus/truecase/${corpus_truecase}.${src}
-$train_truecaser --model truecase-model.${target} --corpus ${TM_ROOT}/corpus/truecase/${corpus_truecase}.${target}
+#cd ${TM_ROOT}/corpus/truecase/
+#check_file ${CORPUS_ROOT}/truecase/${corpus_truecase}.${src} "truecase corpus ${src}"
+#check_file ${CORPUS_ROOT}/truecase/${corpus_truecase}.${tgt} "truecase corpus ${tgt}"
+#cp ${CORPUS_ROOT}/truecase/${corpus_truecase}.${src} .
+#cp ${CORPUS_ROOT}/truecase/${corpus_truecase}.${tgt} .
+#
+## train truecase model.
+#cd ${TM_ROOT}/truecase-model/
+#$train_truecaser --model truecase-model.${src} --corpus ${TM_ROOT}/corpus/truecase/${corpus_truecase}.${src}
+#$train_truecaser --model truecase-model.${tgt} --corpus ${TM_ROOT}/corpus/truecase/${corpus_truecase}.${tgt}
 
 #cd ${TM_ROOT}/corpus/lm/
 #check_file ${CORPUS_ROOT}/lm/${corpus_lm}.${src} "lm corpus ${src}"
-#check_file ${CORPUS_ROOT}/lm/${corpus_lm}.${target} "lm corpus ${target}"
+#check_file ${CORPUS_ROOT}/lm/${corpus_lm}.${tgt} "lm corpus ${tgt}"
 #cp ${CORPUS_ROOT}/lm/${corpus_lm}.${src} .
-#cp ${CORPUS_ROOT}/lm/${corpus_lm}.${target} .
+#cp ${CORPUS_ROOT}/lm/${corpus_lm}.${tgt} .
 
 cd ${TM_ROOT}/corpus/training/
 check_file ${CORPUS_ROOT}/training/${corpus_training}.${src} "training corpus ${src}"
-check_file ${CORPUS_ROOT}/training/${corpus_training}.${target} "training corps ${target}"
+check_file ${CORPUS_ROOT}/training/${corpus_training}.${tgt} "training corps ${tgt}"
 cp ${CORPUS_ROOT}/training/${corpus_training}.${src} corpus.tok.${src}
-cp ${CORPUS_ROOT}/training/${corpus_training}.${target} corpus.tok.${target}
+cp ${CORPUS_ROOT}/training/${corpus_training}.${tgt} corpus.tok.${tgt}
 
-# truecase training corpus.
-cd ${TM_ROOT}/corpus/training/
-$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${src} < corpus.tok.${src} > corpus.true.${src}
-$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${target} < corpus.tok.${target} > corpus.true.${target}
+## truecase training corpus.
+#cd ${TM_ROOT}/corpus/training/
+#$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${src} < corpus.tok.${src} > corpus.true.${src}
+#$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${tgt} < corpus.tok.${tgt} > corpus.true.${tgt}
+cp corpus.tok.${src} corpus.true.${src}
+cp corpus.tok.${tgt} corpus.true.${tgt}
+cp corpus.true.${src} corpus.clean.${src}
+cp corpus.true.${tgt} corpus.clean.${tgt}
 
 # Everything lowercased.  TODO: still right for truecase ?
-
-# clean training corpus.
-
-# One sentence per line, no empty lines.
-# Sentences longer than 100 words (and their corresponding translations) 
-# have to be eliminated (note that a shorter sentence length limit will speed up training.
-
-# TODO: read the source code of clean_corpus_n
-# clean_corpus_n perform the following steps:
-# 1. removes empty lines
-# 2. removes redundant space characters
-# 3. drops lines (and their corresponding lines), that are empty, too short, 
-# too long or violate the 9-1 sentence ratio limit of GIZA++ 
-$clean_corpus_n corpus.true ${src} ${target} corpus.clean 1 80
-
-# prepare tuning corpus.
-# ----------------------
-cd ${TM_ROOT}/corpus/tuning
-check_file ${CORPUS_ROOT}/tuning/${corpus_tuning}.${src} "tuning corpus ${src}"
-check_file ${CORPUS_ROOT}/tuning/${corpus_tuning}.${target} "tuning corps ${target}"
-
-cp ${CORPUS_ROOT}/tuning/${corpus_tuning}.${src} corpus_tuning.tok.${src}
-cp ${CORPUS_ROOT}/tuning/${corpus_tuning}.${target} corpus_tuning.tok.${target}
-$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${src} < corpus_tuning.tok.${src} > corpus_tuning.true.${src}
-$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${target} < corpus_tuning.tok.${target} > corpus_tuning.true.${target}
-
-# prepare evaluation corpus.
-# --------------------------
-cd ${TM_ROOT}/corpus/evaluation
-check_file ${CORPUS_ROOT}/evaluation/${corpus_eval}.${src} "eval corpus ${src}"
-check_file ${CORPUS_ROOT}/evaluation/${corpus_eval}.${target} "eval corps ${target}"
-
-cp ${CORPUS_ROOT}/evaluation/${corpus_eval}.${src} corpus_eval.tok.${src}
-cp ${CORPUS_ROOT}/evaluation/${corpus_eval}.${target} corpus_eval.tok.${target}
-$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${src} < corpus_eval.tok.${src} > corpus_eval.true.${src}
-$truecaser --model ${TM_ROOT}/truecase-model/truecase-model.${target} < corpus_eval.tok.${target} > corpus_eval.true.${target}
 
 # Build Language Model and Train Phrase Model
 # ===========================================
 cd ${TM_ROOT}
 # use irstlm to build lm.
-$IRSTLM/bin/add-start-end.sh < corpus/training/corpus.true.${target} > lm/corpus.sb.${target}
+$IRSTLM/bin/add-start-end.sh < corpus/training/corpus.true.${tgt} > lm/corpus.sb.${tgt}
 cd lm
 mkdir tmp
-$IRSTLM/bin/build-lm.sh -i corpus.sb.${target} -t ./tmp -p -s improved-kneser-ney -o corpus.lm.${target}
-$IRSTLM/bin/compile-lm --text yes corpus.lm.${target}.gz corpus.arpa.${target}
+$IRSTLM/bin/build-lm.sh -i corpus.sb.${tgt} -t ./tmp -p -s improved-kneser-ney -o corpus.lm.${tgt}
+$IRSTLM/bin/compile-lm --text yes corpus.lm.${tgt}.gz corpus.arpa.${tgt}
 
 # binarise the output irstlm lm file using KenLM.
-${MOSES_SUITE_ROOT}/moses/bin/build_binary -i corpus.arpa.${target} corpus.blm.${target}
+${MOSES_SUITE_ROOT}/moses/bin/build_binary -i corpus.arpa.${tgt} corpus.blm.${tgt}
 
 # train phrase model
 # --parallel                parallel run some steps of nine steps if multiple processors.
@@ -215,51 +160,21 @@ ${train_model} \
     --model-dir ${TM_ROOT}/model \
     --corpus-dir ${TM_ROOT}/corpus/training/ \
     --corpus ${TM_ROOT}/corpus/training/corpus.clean \
-    --f ${src} --e ${target} \
+    --f ${src} --e ${tgt} \
     --alignment grow-diag-final-and \
     --reordering msd-bidirectional-fe \
     --lm 0:3:${TM_ROOT}/lm/corpus.blm.${target}:8 \
     &> ${TM_ROOT}/training/training.out
 
-# Tuning 
-# ======
-${mert_moses} \
-    ${TM_ROOT}/corpus/tuning/corpus_tuning.true.${src} \
-    ${TM_ROOT}/corpus/tuning/corpus_tuning.true.${target} \
-    $moses ${TM_ROOT}/model/moses.ini \
-    --decoder-flags="-threads 4 -v 0" \
-    --working-dir ${TM_ROOT}/tuning/mert-work \
-    --mertdir $mertdir \
-    --rootdir ${SCRIPTS_ROOT} \
-    &> ${TM_ROOT}/tuning/tuning.out 
-
 # Create binary model
 # ===================
-check_file "${TM_ROOT}/tuning/mert-work/moses.ini" "tuned moses config"
-
 cd ${TM_ROOT}
 mkdir bin-model
 ${processPhraseTable} -ttable 0 0 ${TM_ROOT}/model/phrase-table.gz -nscores 5 -out ${TM_ROOT}/bin-model/phrase-table
 ${processLexicalTable} -in ${TM_ROOT}/model/reordering-table.wbe-msd-bidirectional-fe.gz -out ${TM_ROOT}/bin-model/reordering-table
 
-cp ${TM_ROOT}/tuning/mert-work/moses.ini ${TM_ROOT}/bin-model/moses.ini
-#cp ${TM_ROOT}/model/moses.ini ${TM_ROOT}/bin-model/moses.ini
+cp ${TM_ROOT}/model/moses.ini ${TM_ROOT}/bin-model/moses.ini
 cd bin-model
 sed -i -e "/phrase-table/s|^0|1|" moses.ini
 sed -i -e "s|model/phrase-table.gz|bin-model/phrase-table|" moses.ini
 sed -i -e "s|model/reordering-table.wbe-msd-bidirectional-fe.gz|bin-model/reordering-table|" moses.ini
-
-# Evaluation
-# ==========
-## Filter Transaltion Model according to evaluation corpus
-#if [ -d ${TM_ROOT}/evaluation/filtered-newstest2011 ]; then
-#    rm -rf ${TM_ROOT}/evaluation/filtered-newstest2011
-#fi
-#
-#$filter_model_given_input ${TM_ROOT}/evaluation/filtered-newstest2011 ${TM_ROOT}/tuning/mert-work/moses.ini ${TM_ROOT}/corpus/evaluation/newstest2011.true.fr -Binarizer ${processPhraseTable}
-#
-## Decoding Test Corpus
-#$moses -f ${TM_ROOT}/evaluation/filtered-newstest2011/moses.ini -input-file ${TM_ROOT}/corpus/evaluation/newstest2011.true.fr > ${TM_ROOT}/corpus/evaluation/newstest2011.translated.en 2> ${TM_ROOT}/evaluation/newstest2011.out
-#
-## Scoring
-#${multi_bleu} -lc ${TM_ROOT}/corpus/evaluation/newstest2011.true.en < ${TM_ROOT}/corpus/evaluation/newstest2011.translated.en
