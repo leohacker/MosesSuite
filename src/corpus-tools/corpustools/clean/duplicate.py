40637f3c5bf42de2652fe03e30abce33abb2da25
Leo Jiang <leo.jiang.dev@gmail.com>
2012-07-13 10:49:13 +0800

    add the skeleton file for duplicate and regex.

---
+++ src/corpus-tools/corpustools/clean/duplicate.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+# pylint: disable=C0301,C0111
+
+"""
+Clean the duplicate sentences.
+
+"""
+
+def run(clean, tools, step):
+    # don't implement duplicate removing.
+    pass
---
+++ src/corpus-tools/corpustools/clean/regex.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+def run(clean, tools, step):
+    # parse the step first.
+    # create the re object.
+    pass
